776d53b9d51075d28f24c614cc818b9bb388a700
comalvirdi <comalvirdi@gmail.com>
2020-10-23 14:53:53 -0700

    create migration, create model for leads table, initialize sequelize

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-modules.exports = {
+module.exports = {
   "development": {
     "username": "express-mvp-dbuser",
     "password": '123',
-    "database": "express_mvp_db",
+    "database": "express-mvp-db",
     "host": "127.0.0.1",
     "dialect": "postgres",
     "port" : 5432
